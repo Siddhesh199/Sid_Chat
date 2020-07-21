f77fb5d23f4627d398dc6ab977896915464800d8
Siddhesh199 <siddheshsalaskar188@gmail.com>
2020-07-21 18:57:15 +0530

    Added Streambuilder

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,25 +5,29 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) => Container(
-          padding: EdgeInsets.all(8.0),
-          child: Text('Hello'),
-        ),
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/bQpu0Jz1Ki9ZITmph9Y5/messages')
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = snapshot.data.documents;
+          return ListView.builder(
+            itemCount: snapshot.data.documents.length,
+            itemBuilder: (context, index) => Container(
+              padding: EdgeInsets.all(8.0),
+              child: Text(documents[index]['text']),
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {
-          Firestore.instance
-              .collection('chats/bQpu0Jz1Ki9ZITmph9Y5/messages')
-              .snapshots()
-              .listen((data) {
-            data.documents.forEach((document) {
-              print(document['text']);
-            });
-          });
-        },
+        onPressed: () {},
       ),
     );
   }
